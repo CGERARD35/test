b8d6c670b5538d023c9f35e0158c80b9e565b57b
Carl GERARD <carl.gerard@epsi.fr>
2022-05-26 09:04:51 +0200

    3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,7 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [BrowserModule],
+  providers: [BrowserModule,AppModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
